1a76a82fcb0f43e2fcd93b979e13addc89febf6b
Prasad24909 <durgaprasad24a@gmail.com>
2023-02-18 17:11:21 +0530

    Solution

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,11 +19,24 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        //Change password only if the oldPassword is equal to current password and the new password meets all the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword == getPassword() && newPassword.length() >= 8){
+            int upper = 0,lower =0, digit =0,special =0;
+            for(int i=0;i<newPassword.length();i++){
+                char c = newPassword.charAt(i);
+                if(c >= 65 && c <= 90) upper++;
+                else if(c>=97 && c<=122) lower++;
+                else if (c>=48 && c<=57) digit++;
+                else special++;
+            }
+            if(upper >=1 && lower >=1 && digit >=1 && special>=1){
+                this.password = newPassword;
+            }
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,19 +5,21 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 public class Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
-        // The inboxCapacity is equal to the maximum value an integer can store.
-
+        // The inboxCapacity is equal to the maximum value an integer can store.;
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +27,31 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        int maxMeetings = 0;
 
+        for (Meeting meeting1 : this.calendar) {
+            int count = 1;
+
+            for (Meeting meeting2 : this.calendar) {
+                if (meeting1 == meeting2) {
+                    continue;
+                }
+
+                if (meeting1.getStartTime().isBefore(meeting2.getEndTime()) &&
+                        meeting1.getEndTime().isAfter(meeting2.getStartTime())) {
+                    // Meetings overlap, so can't attend both
+                    count = 1;
+                    break;
+                } else {
+                    count++;
+                }
+            }
+
+            if (count > maxMeetings) {
+                maxMeetings = count;
+            }
+        }
+
+        return maxMeetings;
     }
 }
